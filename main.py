1f3b8215cfe2a8db05da1e9ee66644e8f3b9f193
Gustavo Madeira <147175175+Gustavo-Madeira@users.noreply.github.com>
2024-07-24 11:28:42 -0300

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-class temperauteConverter:
+class Temperaute:
     def __init__(self):
         pass
     def kelvin(self, c):
         return c + 273
     def fahrenheit(self, c):
-        return (((c * 9) / 5) + 32)
+        return ((c / 5) * 9) + 32
+calcular = Temperaute()
 
-calcular = temperauteConverter()
-c = float(input('Digite a temperatura em °C: '))
+print(f'Qual temperatura voce quer converter? \n1- Kelvin \n2- Fahrenheit')
+opcao = input('Escreva o numero daquilo que deseja calcular: ')
 
-print(f'{c}°C, convertendo, vira {calcular.fahrenheit(c)}°F e/ou {calcular.kelvin(c)}')
+if opcao == '1':
+    c = float(input('Digite a temperatura em graus Celsius: '))
+    print(f'{c}°C vira {calcular.kelvin(c)}K')
+elif opcao == '2':
+    c = float(input('Digite a temperatura em graus Celsius: '))
+    print(f'{c}°C vira {calcular.fahrenheit(c)}°F')
